20b06e2722f71e4bf3d640e86c317148b93e622a
Oskar Jönsson <oskar.jonsson@kivra.com>
2021-07-08 11:36:25 +0200

    Setup NetworkingManager

--- FlickerSearch/API/NetworkManager.swift
+++ FlickerSearch/API/NetworkManager.swift
@@ -8,12 +8,30 @@
 import Foundation
 
 protocol NetworkManagerProtocol {
-    func fetchData<T: Decodable>(request: URLRequest, model: T.Type, completion: (_ result: Result<T, Error>) -> Void)
+    func fetchData<T: Decodable>(request: URLRequest, model: T.Type, completion: @escaping(_ result: Result<T, Error>) -> Void)
 }
 
 class NetworkManager: NetworkManagerProtocol {
 
-    func fetchData<T: Decodable>(request: URLRequest, model: T.Type, completion: (_ result: Result<T, Error>) -> Void) {
+    func fetchData<T: Decodable>(request: URLRequest, model: T.Type, completion: @escaping(_ result: Result<T, Error>) -> Void) {
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: request) { data, response, error in
 
+            if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(T.self, from: data)
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else {
+                completion(.failure(error ?? NetworkError.genericError))
+            }
+        }
+    }
+
+    enum NetworkError: Error {
+        case genericError
     }
 }
